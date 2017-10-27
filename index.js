3df13dc041990328d378b05db4f7c378b14cb8b6
axetroy <troy450409405@gmail.com>
2017-10-27 17:38:09 +0800

    2th commit

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const git = require('simple-git')(__dirname);
 const META_FILE_PATH = './meta.json';
 
 async function main() {
-  const meta = fs.readFileSync(META_FILE_PATH);
+  const meta = JSON.parse(
+    fs.readFileSync(META_FILE_PATH, { encoding: 'utf8' })
+  );
 
   await new Promise((resolve, reject) => {
     git.pull((err, update) => {
@@ -15,10 +17,12 @@ async function main() {
   meta.times = meta.times + 1;
   meta.updatedAt = new Date();
 
-  fs.writeFileSync(META_FILE_PATH, JSON.stringify(meta, null, 2));
+  fs.writeFileSync(META_FILE_PATH, JSON.stringify(meta, null, 2), {
+    encoding: 'utf8'
+  });
 
   await new Promise((resolve, reject) => {
-    git.commit(`${meta.times}th commit`, (err, data) => {
+    git.add('./').commit(`${meta.times}th commit`, (err, data) => {
       err ? reject(err) : resolve(data);
     });
   });
