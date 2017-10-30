216ffc9d3ddec7127f136f5c644cb32e23cf5850
axetroy <troy450409405@gmail.com>
2017-10-30 10:17:43 +0800

    update

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
+const chalk = require('chalk');
+
+console.info(`process ${chalk.blue(process.pid)} ${chalk.green('start')}.`);
+
 process.on('SIGINT', () => {
-  console.error(new Error(`Exist sigint`));
+  console.info(`SIGINT received`);
   process.exit(1);
 });
-process.on('exit', function() {
-  console.info(`process ${process.pid} exit.`);
+
+process.on('exit', () => {
+  console.info(`process ${chalk.blue(process.pid)} ${chalk.red('exit')}.`);
 });
-process.on('uncaughtException', function(err) {
-  console.error('Error caught in uncaughtException event:', err);
+
+process.on('uncaughtException', err => {
+  console.error('Error caught in uncaughtException event:');
+  console.error(err);
 });
 
 process.on('unhandledRejection', (reason, p) => {
   console.error('Unhandled Rejection at:', p, 'reason:', reason);
 });
 
-const fs = require('fs');
+const fs = require('fs-extra');
+const _ = require('lodash');
+const moment = require('moment');
 const git = require('simple-git')(__dirname);
 const schedule = require('node-schedule');
 const emoji = require('./emojis.json');
 const emojiKeys = Object.keys(emoji);
-const emojiNumber = emojiKeys.length;
 
 const META_FILE_PATH = './meta.json';
 
+/**
+ * get random emoji char
+ * @returns {*}
+ */
 function getEmoji() {
-  const index = parseInt(Math.random() * emojiNumber);
-  if (!emoji[index]) {
+  const index = _.random(0, emojiKeys.length);
+  const key = emojiKeys[index];
+  const entity = emoji[key];
+  if (!entity.char) {
     return getEmoji();
   } else {
-    const key = emojiKeys[index];
-    const entity = emoji[key];
-    if (!entity.char) {
-      return getEmoji();
-    } else {
-      return entity.char;
-    }
+    return entity.char;
   }
 }
 
-async function main() {
-  const meta = JSON.parse(
-    fs.readFileSync(META_FILE_PATH, { encoding: 'utf8' })
-  );
+/*
+* commit the project
+* */
+async function commit() {
+  const meta = await fs.readJson(META_FILE_PATH, { encoding: 'utf8' });
 
   await new Promise((resolve, reject) => {
     git.pull((err, update) => {
@@ -51,22 +60,31 @@ async function main() {
   meta.times = meta.times + 1;
   meta.updatedAt = new Date();
 
-  fs.writeFileSync(META_FILE_PATH, JSON.stringify(meta, null, 2), {
-    encoding: 'utf8'
-  });
+  await fs.writeJson(META_FILE_PATH, meta, { encoding: 'utf8' });
 
   await new Promise((resolve, reject) => {
     git
       .add('./')
-      .commit(`${meta.times}th commit ${getEmoji()}`)
+      .commit(
+        `[${moment(meta.updatedAt).format(
+          `YYYY-MM-DD HH:mm:ss`
+        )}] ${meta.times}th commit ${getEmoji()}`
+      )
       .push((err, data) => {
         err ? reject(err) : resolve(data);
       });
   });
 }
 
-// schedule.scheduleJob('* */1 * * * *', function() {
-schedule.scheduleJob('* * */1 * * *', function() {
-  // 每隔1分钟commit一次
-  main();
+// 没隔1小时commit一次
+// 不过分吧？
+// schedule.scheduleJob('0 0 */1 * * *', function() {
+schedule.scheduleJob('0 */1 * * * *', function() {
+  commit()
+    .then(() => {
+      console.info(`Commit success`);
+    })
+    .catch(err => {
+      console.error(err);
+    });
 });
